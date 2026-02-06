0099f11e8eb306bcefb928e6a3d8316954a2a2ac
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-02-06 20:26:21 +0530

    Add dotenv configuration for environment variables

--- back/ai.js
+++ back/ai.js
@@ -1,6 +1,8 @@
 import Groq from "groq-sdk";
 import { MongoClient } from "mongodb";
 // import { embn } from "./set.js";
+import 'dotenv/config';
+
 import { Pinecone } from "@pinecone-database/pinecone";
 const client = new MongoClient(process.env.MONGO_URI);
 await client.connect();
