5b642c761e661b80389d7d4bb59f2955a441a90e
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-03-03 04:19:03 +0530

    Update auth.js

--- back/auth.js
+++ back/auth.js
@@ -1,28 +1,17 @@
-// server/auth.js
 import jwt from "jsonwebtoken";
-
-const SECRET = "MY_SECRET_KEY"; // 🔒 server की secret
-
+const SECRET = "MY_SECRET_KEY"; 
 export function auth(req, res, next) {
   const header = req.headers.authorization;
-
-  // token नहीं आया
   if (!header) {
     return res.status(401).json({ message: "No token" });
   }
-
-  const token = header.split(" ")[1]; // Bearer TOKEN
-
+  const token = header.split(" ")[1];  
   try {
-    // 🔥 VERIFY
     const decoded = jwt.verify(token, SECRET);
-
-    // decoded = { userId, email, iat, exp }
     req.user = decoded;
 
-    next(); // route को जाने दो
+    next();
   } catch (err) {
-    // return  res.status("fuck...")
     return res.send({ok:false});
   }
 }
